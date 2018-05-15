0915eec8e054d0b475ed51dcf3e3f2c4996c06ad
Rabbit <himasankar.tikka@devrabbit.com>
2018-05-15 14:35:21 +0530

    Committing changes

---
+++ Samples/BasiciOSNativeApplication/BasiciOSNativeApplication/Views/MultiViewsToController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace BasiciOSNativeApplication
+{
+    public class MultiViewsToController : UIViewController
+    {
+        public nfloat screenHeight, screenWidth;
+        public MultiViewsToController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            screenHeight = (View.Bounds.Height) / 100;
+            screenWidth = (View.Bounds.Width) / 100;
+
+            UITextField inputUserName = new UITextField();
+            inputUserName.Frame = new CGRect((screenWidth * 10), (screenHeight * 30), (screenWidth * 80), (screenHeight * 10));
+            //inputUserName.BackgroundColor = UIColor.Yellow;
+            inputUserName.BorderStyle = UITextBorderStyle.RoundedRect;
+
+
+            UITextField inputUserPassword = new UITextField()
+            {
+                BackgroundColor = UIColor.Yellow,
+                SecureTextEntry = true,//this is similar to ispassword in xamarin.forms
+                Frame = new CGRect((screenWidth * 10), (screenHeight * 45), (screenWidth * 80), (screenHeight * 10)),
+                BorderStyle = UITextBorderStyle.RoundedRect
+            };
+
+
+            //UIButton buttonSubmit = new UIButton();
+            UIButton buttonSubmit = UIButton.FromType(UIButtonType.RoundedRect);
+            buttonSubmit.SetTitle("Submit", UIControlState.Normal);
+            buttonSubmit.SetTitleColor(UIColor.Green, UIControlState.Normal);
+            buttonSubmit.Frame = new CGRect((screenWidth * 10), (screenHeight * 60), (screenWidth * 80), (screenHeight * 10));
+            buttonSubmit.BackgroundColor = UIColor.Yellow;
+
+            View.BackgroundColor = UIColor.Gray;
+            View.AddSubviews(new UIView[]{inputUserName, inputUserPassword, buttonSubmit});
+
+        }
+    }
+}
---
+++ Samples/BasiciOSNativeApplication/BasiciOSNativeApplication/Views/SecondTrailViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace BasiciOSNativeApplication
+{
+    public class SecondTrailViewController : UIViewController
+    {
+        public SecondTrailViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.Yellow;
+            Title = "Second View Controller";
+
+            UIButton buttonOpenNewControllerOne = UIButton.FromType(UIButtonType.System);
+            buttonOpenNewControllerOne.Frame = new CGRect(50, 100, 150, 40);
+            buttonOpenNewControllerOne.SetTitle("First Button", UIControlState.Normal);
+
+
+            UIButton buttonOpenNewControllerTwo = new UIButton(new CGRect(50, 200, 150, 40));
+            buttonOpenNewControllerTwo.SetTitle("Second Button", UIControlState.Normal);
+            buttonOpenNewControllerTwo.SetTitleColor(UIColor.Green, UIControlState.Normal);
+
+
+
+
+            UIViewController naviPage = new UIViewController();
+            naviPage.View.BackgroundColor = UIColor.Orange;
+
+            var multiView = new MultiViewsToController();
+
+
+
+
+            buttonOpenNewControllerOne.TouchUpInside += async (object sender, EventArgs e) => 
+            {
+                this.NavigationController.PushViewController(naviPage, true);
+            };
+
+            buttonOpenNewControllerTwo.TouchUpInside += async (object sender, EventArgs e) => 
+            {
+                this.NavigationController.PushViewController(multiView, true);
+            };
+
+            View.AddSubview(buttonOpenNewControllerOne);
+            View.AddSubview(buttonOpenNewControllerTwo);
+
+
+        }
+    }
+}
